27ff9620676084a48c86cbf9f2f514cb0f601d99
rry <r.yamakuchi@gmail.com>
2019-12-12 04:13:26 +0900

    add: calendar

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,166 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        advent-calendar-2019-bulma
-      </h1>
-      <h2 class="subtitle">
-        My praiseworthy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
+  <section class="section container content">
+    <h1>
+      Nuxt.js Advent Calendar 2019
+    </h1>
+
+    <p>{{ comment }}</p>
+
+    <table class="table is-bordered">
+      <thead>
+        <tr>
+          <th
+            v-for="dayName in dayNames"
+            :key="dayName"
+            class="week"
+          >
+            {{ dayName }}
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr
+          v-for="index in [0, 1, 2]"
+          :key="index"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+          <th
+            v-for="day in days.slice((7 * index), (7 + (7 * index)))"
+            :key="day.date"
+            class="week"
+          >
+            <p class="tag is-primary is-light is-medium">
+              {{ day.date }}
+            </p>
+
+            <article class="media">
+              <div class="media-content">
+                <img
+                  class="image is-48x48"
+                  :src="day.authorImageUrl"
+                >
+
+                <a
+                  :href="`https://qiita.com/${day.authorName}`"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="has-text-weight-normal is-size-6 has-text-info"
+                >
+                  {{ day.authorName }}
+                </a>
+
+                <div class="media content">
+                  <a v-if="day.articleUrl !== null"
+                    :href="day.articleUrl"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="has-text-info is-6"
+                  >
+                    {{ day.comment }}
+                  </a>
+
+                  <p v-else
+                    class="has-text-dark is-6"
+                  >
+                    {{ day.comment }}
+                  </p>
+                </div>
+              </div>
+            </article>
+          </th>
+        </tr>
+
+        <tr>
+          <th
+            v-for="day in days.slice(21)"
+            :key="day.date"
+            class="week"
+          >
+            <p class="tag is-primary is-light is-medium">
+              {{ day.date }}
+            </p>
+
+            <article class="media">
+              <div class="media-content">
+                <img
+                  class="image is-48x48"
+                  :src="day.authorImageUrl"
+                >
+
+                <a
+                  :href="`https://qiita.com/${day.authorName}`"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="has-text-weight-normal is-size-6 has-text-info"
+                >
+                  {{ day.authorName }}
+                </a>
+
+                <div class="media content">
+                  <a v-if="day.articleUrl !== null"
+                    :href="day.articleUrl"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="has-text-info is-6"
+                  >
+                    {{ day.comment }}
+                  </a>
+
+                  <p v-else
+                    class="has-text-dark is-6"
+                  >
+                    {{ day.comment }}
+                  </p>
+                </div>
+              </div>
+            </article>
+          </th>
+
+          <th
+            v-for="date in [26, 27, 28]"
+            :key="date"
+          >
+            <p class="tag is-light is-medium">
+              {{ date }}
+            </p>
+          </th>
+        </tr>
+      </tbody>
+    </table>
+
+    <p>※ このページはレスポンシブ対応していません。デスクトップサイズの画面でご覧ください。</p>
+  </section>
 </template>
 
+<script>
+import { parse } from 'node-html-parser'
+
+export default {
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get('https://qiita.com/advent-calendar/2019/nuxt-js')
+    const root = parse(data)
+    return {
+      comment: root.querySelector('.markdownContent').text,
+      dayNames: root.querySelectorAll('.adventCalendarCalendar_dayName').map(dayName => dayName.text),
+      days: root.querySelectorAll('.adventCalendarCalendar_day').map(day => {
+        const articleUrl = day.querySelector('.adventCalendarCalendar_comment').querySelector('a') ?
+          day.querySelector('.adventCalendarCalendar_comment').querySelector('a').attributes['href'] :
+          null
+        return {
+          date: day.querySelector('.adventCalendarCalendar_date').text,
+          authorName: day.querySelector('.adventCalendarCalendar_author').text.replace(/\s|&nbsp;/g, ''),
+          authorImageUrl: day.querySelector('.adventCalendarCalendar_authorIcon').attributes['src'],
+          comment: day.querySelector('.adventCalendarCalendar_comment').text,
+          articleUrl: articleUrl
+        }
+      })
+    }
+  }
+}
+</script>
+
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.week {
+  width: calc(100% / 7);
 }
 </style>
